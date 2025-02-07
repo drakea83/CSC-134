caadbb24548fe1a84c56d597cb4775427a0d5341
drakea83 <aaron.drake.83@gmail.com>
2025-02-07 16:02:09 +0000

    M2T1_Drake started

---
+++ M2T1_Drake.cpp
@@ -0,0 +1,15 @@
+// CSC-134
+// M2T1
+// Aaron Drake 
+// 7 Feb 2025
+
+# include <iostream>
+using namespace std;
+
+int main() {
+    
+    cout << "Hello World!" << endl;
+cout << endl;
+ 
+    return 0;
+}
