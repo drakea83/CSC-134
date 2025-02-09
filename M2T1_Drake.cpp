8b8fcde4bdf31faffa75d351bc0a0b5e46b2cc13
drakea83 <aaron.drake.83@gmail.com>
2025-02-09 16:47:08 -0500

    M2T1_Drake.cpp
    Finished assignment

--- M2T1_Drake.cpp
+++ M2T1_Drake.cpp
@@ -12,7 +12,13 @@ int main() {
     double price_each = 0.25;
     
     cout << "Welcome to the " << name << " Apple Farm!" << endl;
-cout << endl;
+	cout << "We have " << num_apples << " apples in stock." << endl;
+    cout << "Each apple costs " << price_each << endl;
  
+ double total_price = num_apples * price_each;
+ 	cout << "" << endl;
+ 	cout << "The total price for all " << num_apples;
+    cout << " is: $" << total_price << "0" << endl;
+    cout << "Thanks for coming by!" << endl;
     return 0;
 }
