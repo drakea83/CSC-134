5dd3e3b19d14a7aa2a5471b741cba53ffe6947f8
drakea83 <aaron.drake.83@gmail.com>
2025-02-09 16:19:18 -0500

    Update M2T1_Drake.cpp

--- M2T1_Drake.cpp
+++ M2T1_Drake.cpp
@@ -1,14 +1,17 @@
 // CSC-134
 // M2T1
 // Aaron Drake 
-// 7 Feb 2025
+// 9 Feb 2025
 
 # include <iostream>
 using namespace std;
 
 int main() {
+    string name = "Drake";
+    int num_apples = 10;
+    double price_each = 0.25;
     
-    cout << "Hello World!" << endl;
+    cout << "Welcome to the " << name << " Apple Farm!" << endl;
 cout << endl;
  
     return 0;
