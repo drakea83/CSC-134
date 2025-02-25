b3382fcce74c65f5b48590862e39e82698ea0fff
drakea83 <aaron.drake.83@gmail.com>
2025-02-25 18:55:45 +0000

    started

---
+++ M2HW1_Q2_Drake.cpp
@@ -0,0 +1,41 @@
+// CSC-134
+// M2HW1 - Gold
+// Question 2
+// Aaron Drake 
+// 25 Feb 2025
+
+# include <iostream>
+using namespace std;
+
+int main() {
+    const double cost_per_cubic_foot = 0.3;
+    const double charge_per_cubic_foot = 0.52;
+
+    double length;
+    double width;
+    double height;
+    double volume;
+    double cost;
+    double charge;
+    double profit;
+
+    cout << "Enter the dimensions of the crate (in feet):/n";
+    cout << "Length: ";
+    cin >> length;
+    cout << "Width: ";
+    cin >> width;
+    cout << "Height: ";
+    cin >> height;
+
+    volume = length * width * height;
+    cost = volume * cost_per_cubic_foot;
+    charge = volume * charge_per_cubic_foot;
+    profit = cost - charge;
+
+    cout << "The volume of the crate is ";
+    cout << volume << " cubic feet./n";
+    cout << "Cost to build: $" << cost << endl;
+    cout << "Charge to customer: $" << charge << endl;
+    cout << "Profit: $" << abs(profit) << endl;
+    return 0;
+}
