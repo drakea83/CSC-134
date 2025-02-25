bee598dda52d97ef39fda90739b44f85c042cbed
drakea83 <aaron.drake.83@gmail.com>
2025-02-25 20:16:04 +0000

    finished Q4

--- M2HW1_Q4_Drake.cpp
+++ M2HW1_Q4_Drake.cpp
@@ -5,17 +5,20 @@
 // 25 Feb 2025
 
 # include <iostream>
+# include <string>
 using namespace std;
 
 int main() {
-    string school = FTCC; 
-    string CheerOne = "Let's go";
-    string team = Trojans;
+    string school = "FTCC"; 
+    string letsgo = "Let's go";
+    string team = "Trojans";
+    string cheerOne = letsgo + " " + school;
+    string cheerTwo = letsgo + " " + team;
 
-    cout << CheerOne << " " << school << endl;
-    cout << CheerOne << " " << school << endl;
-    cout << CheerOne << " " << school << endl;
-    cout << CheerOne << " " << team << endl;
-
+    cout << cheerOne << endl;
+    cout << cheerOne << endl;
+    cout << cheerOne << endl;
+    cout << cheerTwo << endl;
+    
     return 0;
 }
