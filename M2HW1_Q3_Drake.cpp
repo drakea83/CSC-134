8cf1c84945eabedf531a9f90eefff79ccfbaedea
drakea83 <aaron.drake.83@gmail.com>
2025-02-25 19:18:44 +0000

    finished

--- M2HW1_Q3_Drake.cpp
+++ M2HW1_Q3_Drake.cpp
@@ -17,17 +17,17 @@ int main() {
     cout << "How many pizzas did you order?" << endl;
     cin >> number_pizzas;
     cout << "How many slices are there in each pizza?" << endl;
-    cin >> slices_per_pizza
+    cin >> slices_per_pizza;
     cout << "How many people were invited to the party?" << endl;
     cin >> number_people;
 
     double total_number_slices = number_pizzas * slices_per_pizza;
     double number_pizza_slices_consumed = number_people * slices_per_person;
     double remaining_slices = total_number_slices - number_pizza_slices_consumed;
-    double remaining_pizza = remaining_slices * slices_per_pizza;
+    double remaining_pizza = remaining_slices / slices_per_pizza;
 
     cout << "The total number of pizzas that will be left over is: " << remaining_pizza << " " << endl;
     cout << "The total number of pizza slices that will be left over is: " << remaining_slices << " " << endl;
-    
+
     return 0;
 }
