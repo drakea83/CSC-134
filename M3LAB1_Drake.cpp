36408c3211ce6a42632ddb38fcc68ea4e83b7ef9
drakea83 <aaron.drake.83@gmail.com>
2025-03-02 23:01:12 -0500

    Update M3LAB1_Drake.cpp
    Added additional choices.

--- M3LAB1_Drake.cpp
+++ M3LAB1_Drake.cpp
@@ -10,8 +10,8 @@ int main() {
 
   int choice;
   int choice_2;
-  int fight;
-  int run;
+  int choice_3;
+  int choice_4;
 
   cout << "This is a combat simulation. You are a soldier in Iraq searching for the enemy." << endl;
   cout << "Choose what building to search." << endl;
@@ -35,6 +35,27 @@ int main() {
   if (2 == choice) {
   	cout << "You chose building 2" << endl;
   	cout << "After a thorough search, the building is empty." << endl;
+  	cout << "Do you hold your position and secure building 2 or do you search building 1?" << endl;
+  	cout << "Type 3 to hold your position or type 4 to search building 1." << endl;
+  	cin >> choice_3;
+
+  	if (3 == choice_3) {
+        cout << "You secure the building and radio for reinforcements to assist you." << endl;
+        cout << "Reinforcements take fire from building 1 and sustain 1 friendly seriously wounded." << endl;
+  	}
+  	if (4 == choice_3) {
+        cout << "You move to building 1 and open the door." << endl;
+        cout << "You find 3 enemy soldiers. Do you fight or run?" << endl;
+  	cout << "Type 7 to fight the soldiers or type 8 to run away." << endl;
+  	cin >> choice_2;
+
+  	if (7 == choice_2){
+        cout << "You fight 3 soldiers single-handly. Your courage overcomes the odds. Victory is yours!" << endl;
+  	}
+  	if (8 == choice_2) {
+        cout << "You flee from the building. The enemy soldiers pursue you. You are defeated." << endl;
+  	}
+  	}
   }
 
   cout << "Thanks for playing!" << endl;
