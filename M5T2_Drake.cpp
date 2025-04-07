87b548077a5bf2585e816755889294754267af90
drakea83 <aaron.drake.83@gmail.com>
2025-04-06 23:38:24 -0400

    Create M5T2_Drake.cpp

---
+++ M5T2_Drake.cpp
@@ -0,0 +1,31 @@
+// CSC-134
+// M5T2
+// Aaron Drake
+// 6 Apr 2025
+
+#include <iostream>
+using namespace std;
+
+void printResult(int number, int result){
+cout << number << " squared = " << result << endl;
+}
+
+int square(int number){
+    int result;
+    result = number * number;
+    return result;
+}
+
+int main()
+{
+    int count = 1;
+    int result;
+
+   while (count <= 10){
+    result = square(count);
+    printResult (count, result);
+    count++;
+   }
+
+    return 0;
+}
