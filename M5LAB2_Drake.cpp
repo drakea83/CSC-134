f899d86689022ec87ad252e1875aee6fdaa59978
drakea83 <aaron.drake.83@gmail.com>
2025-04-13 20:24:10 -0400

    Update M5LAB2_Drake.cpp

--- M5LAB2_Drake.cpp
+++ M5LAB2_Drake.cpp
@@ -1,1 +1,26 @@
+// CSC-134
+// M5LAB2
+// Aaron Drake
+// 13 Apr 2025
+
+#include <iostream>
+
+using namespace std;
+
+void main_menu();
+void choice_front_door();
+void choice_back_door();
+void choice_go_home();
+
+int main(){
+    cout << "M5LAB1: Choose Your Own Adventure" << endl;
+    main_menu();
+    cout << "Thanks for playing. I hope you enjoyed the game." << endl;
+    return 0;
+}
+
+void main_menu(){
+    cout << "Main Menu" << endl;
+    cout << ""
+}
 
